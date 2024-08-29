1a62d9a9bc0219fb1dfb7eb473c543adcac2782d
Carlos Boquia <cboquia@academic.rrc.ca>
2024-08-29 00:05:08 -0500

    added unittesting

--- library_item/library_item.py
+++ library_item/library_item.py
@@ -1,5 +1,68 @@
 """"
 Description: A class to manage LibraryItem objects.
-Author: {Student Name}
-Date: {Date}
+Author: Carlos Boquia
+Date: August 28 2024
 """
+from genre.genre import Genre
+
+class LibraryItem():
+    """
+    Represents the items within libary
+
+    Attributess:
+        title (str): The title of the book.
+        author (str): The author of the book.
+        genre (Genre): The genre of the book.
+    
+    """
+
+    def __init__(self, title:str, author:str, genre: Genre):
+
+        """
+        Grabs the title, author and genre of a book from the library
+        and updatess the inventory. 
+
+        Args:
+            title (str): The title of the book.
+            author (str): The author of the book.
+            genre (Genre): The genre of the book.
+        Returns:
+            self.__title (str): Returns private title attribute.
+            self.__author(str): Returns private author attribute.
+            self.__genre (Genre): Returns private genre attribute.
+        Raises:
+            ValueError: "Title cannot be blank"
+            ValueError: "Author cannot be blank"
+            ValueError: "Invalid Genre"
+        """
+
+        if len(title.strip()) == 0:
+            raise ValueError ("Title cannot be blank")
+        else:
+            self.__title = title
+        
+        if len(author.strip()) == 0:
+            raise ValueError ("Author cannot be blank")
+        else:
+            self.__author = author
+        
+        if isinstance(genre, Genre):
+            self.__genre = genre
+        else:
+            raise ValueError("Invalid Genre")
+        
+
+    # Accessors
+
+    @property
+    def title(self) -> str:
+        return self.__title
+        
+    @property
+    def author(self) -> str:
+        return self.__author
+    
+    @property
+    def genre(self) -> Genre:
+        return self.__genre
+        
--- tests/test_library_item.py
+++ tests/test_library_item.py
@@ -1,10 +1,28 @@
 """
 Description: Unit tests for the Book class.
-Author: {Student Name}
-Date: {Date}
+Author: Carlos Boquia
+Date: August 28 2024
 Usage: To execute all tests in the terminal execute 
 the following command:
-    python -m unittest tests/test_book.py
+    python -m unittest tests/test_library_item.py
 """
 
 import unittest
+from genre.genre import Genre
+from library_item.library_item import LibraryItem
+
+class TestClient(unittest.TestCase):
+    def test_init_valid(self):
+        library_item = LibraryItem ("Book", "Bruce Willis", Genre.TRUE_CRIME)
+
+    def test_init_invalid_title(self):
+        with self.assertRaises(ValueError):
+            library_item = LibraryItem(" ", "Bruce Willis", Genre.TRUE_CRIME)
+
+    def test_init_invalid_author(self):
+        with self.assertRaises(ValueError):
+            library_item = LibraryItem ("Book", " ", Genre.TRUE_CRIME)
+
+    def test_init_invalid_genre(self):
+        with self.assertRaises(ValueError):
+            library_item = LibraryItem ("Book", "Bruce Willis", "invalid")
